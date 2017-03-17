3e725cf47e8c0d7b22ea0f3b1c9010df29f71e30
pkim90 <p0179417@gmail.com>
2017-03-17 08:24:08 -0400

    added web template

--- pfpython/entertainmentCenter.py
+++ pfpython/entertainmentCenter.py
@@ -1,16 +1,19 @@
 import media
+import fresh_tomatoes
 
 toyStory = media.Movie("Toy Story", "Stupid kid with shitty imagination", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 print (toyStory.title)
-print (toyStory.story)
+print (toyStory.storyline)
 
 
 avatar = media.Movie("Avatar", "Some blue skank trippin balls", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 print (avatar.title)
-print (avatar.story)
+print (avatar.storyline)
 
 amos = media.Movie("Famous Amos", "Clips of what Amos does", "https://www.facebook.com/photo.php?fbid=3805531532746&set=a.1417468752669.2057224.1111126103&type=3&theater", "https://www.youtube.com/watch?v=jP4DURAnStE")
 
-amos.showTrailer()
+movies = [toyStory, avatar, amos]
+
+fresh_tomatoes.open_movies_page(movies)
--- pfpython/media.py
+++ pfpython/media.py
@@ -7,11 +7,11 @@ def jackie(yag, obo):
 import webbrowser
 
 class Movie():
-    def __init__(self, title, story, poster, trailer):
-        self.title = title
-        self.story = story
-        self.poster = poster
-        self.trailer = trailer
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
     def showTrailer(self):
-        webbrowser.open(self.trailer)
+        webbrowser.open(self.trailer_youtube_url)
